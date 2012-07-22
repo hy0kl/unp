666fe9bf86599d2197c3ba07928f3bf95dba232e
Jerry Yang <hy0kle@gmail.com>
2012-07-22 08:24:31 +0800

    Add makefile.

--- ev-sever/event-http.c
+++ ev-sever/event-http.c
@@ -20,8 +20,8 @@ int get_localtime_str(char *src, size_t buf_len)
     p_tm = localtime(&tm);
 
     snprintf(src, buf_len, "%d-%02d-%02d %02d:%02d:%02d",
-            p_tm->tm_year + 1900, p_tm->tm_mon + 1, p_tm->tm_mday,
-            p_tm->tm_hour, p_tm->tm_min, p_tm->tm_sec);
+        p_tm->tm_year + 1900, p_tm->tm_mon + 1, p_tm->tm_mday,
+        p_tm->tm_hour, p_tm->tm_min, p_tm->tm_sec);
 
     return 0;
 }
